fc13c402b3647bc9f1ed218ef5628743e7d3d971
iusmanof <iusmanof@gmail.com>
2024-05-28 17:07:46 -0700

    feat: implement RTK  countSLicer test

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,37 @@ import "./App.css";
 import FastFoodBlock from "../components/FastFoodBlock";
 import FastFoodItemSkeleton from "../components/FastFoodItemSkeleton";
 import { useEffect, useState } from "react";
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../redux/slices/couterSlice";
 
 function App() {
-  
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <>
       <Header />
 
       <h1>Home</h1>
+
+      <div>
+        <span>{count}</span>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import './index.css'
-import About from '../components/About';
-import Cart from '../components/Cart';
-import Layout from '../components/Layout';
-import NotFound from '../components/NotFound';
-import User from '../components/User';
-import Food from '../components/Food';
-import FastFoodDetail from '../components/FastFoodDetail';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import About from "../components/About";
+import Cart from "../components/Cart";
+import Layout from "../components/Layout";
+import NotFound from "../components/NotFound";
+import User from "../components/User";
+import Food from "../components/Food";
+import FastFoodDetail from "../components/FastFoodDetail";
+
+// RTK
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import counterSlice from "../redux/slices/couterSlice.js";
+
 
 const food = [
-  { id: '1', title: 'food1' },
-  { id: '2', title: 'food2' },
+  { id: "1", title: "food1" },
+  { id: "2", title: "food2" },
 ];
 
 const users = [
-  { id: '1', fullName: 'Robin Wieruch' },
-  { id: '2', fullName: 'Sarah Finnley' },
+  { id: "1", fullName: "Robin Wieruch" },
+  { id: "2", fullName: "Sarah Finnley" },
 ];
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <BrowserRouter>
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
       <Routes element={<Layout />}>
-          <Route index element={<App />}  /> 
-          
-          <Route path="about" element={<About users={users} />} /> 
-          <Route path="about/:userId" element={<User />} />  
+        <Route index element={<App />} />
 
-          <Route path="food" element={<Food/>} /> 
-            {/* <Route path=":foodId" element={<FastFoodDetail />} />
+        <Route path="about" element={<About users={users} />} />
+        <Route path="about/:userId" element={<User />} />
+
+        <Route path="food" element={<Food />} />
+        {/* <Route path=":foodId" element={<FastFoodDetail />} />
           </Route> */}
-          
-          <Route path="food/:foodId" element={<FastFoodDetail />} /> 
 
-          <Route path="cart" element={<Cart />} /> 
-          <Route path="*" element={<NotFound />} />
+        <Route path="food/:foodId" element={<FastFoodDetail />} />
+
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    </Provider>
+  </BrowserRouter>
+);
