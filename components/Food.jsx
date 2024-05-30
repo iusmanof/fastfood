61fc15999ffc4287e4b7bf7175dad377965bb15c
iusmanof <iusmanof@gmail.com>
2024-05-30 12:09:56 -0700

    fix:  axios

--- components/Food.jsx
+++ components/Food.jsx
@@ -8,12 +8,13 @@ import Sort from "../components/Sort";
 import { Outlet } from "react-router-dom";
 import Pagination from "./Pagination";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 export const SearchContext = React.createContext({});
 
 const Food = () => {
-  const categoryFilter  = useSelector((state) => state.filter.category)
-  const sorting = useSelector((state) => state.filter.sorting.property)
+  const categoryFilter = useSelector((state) => state.filter.category);
+  const sorting = useSelector((state) => state.filter.sorting.property);
 
   const [foodState, setFoodState] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -27,17 +28,23 @@ const Food = () => {
   const perPageParam = 2;
 
   useEffect(() => {
-    fetch(
-      `http://localhost:3000/fast-food?category=${categoryParam}&_sort=${sortingParam}&_order=${orderParam}&_page=${pageParam}&_per_page=${perPageParam}`
-      // `http://localhost:3000/fast-food?category=${categoryParam}&_sort=${sortingParam}&_order=${orderParam}`
-    )
-      .then((response) => response.json())
-      .then((data) => setFoodState(data.data));
+    // fetch(
+    //   `http://localhost:3000/fast-food?category=${categoryParam}&_sort=${sortingParam}&_order=${orderParam}&_page=${pageParam}&_per_page=${perPageParam}`
+    //   // `http://localhost:3000/fast-food?category=${categoryParam}&_sort=${sortingParam}&_order=${orderParam}`
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => setFoodState(data.data));
+
+    axios
+      .get(
+        `http://localhost:3000/fast-food?category=${categoryParam}&_sort=${sortingParam}&_order=${orderParam}&_page=${pageParam}&_per_page=${perPageParam}`
+      )
+      .then((res) => setFoodState(res.data.data));
 
     setTimeout(() => {
       setIsLoading(false);
     }, 1000);
-  }, [ sorting, searchValue, page, categoryFilter]);
+  }, [sorting, searchValue, page, categoryFilter]);
 
   const pizzasBLock = foodState
     .filter((obj) => {
@@ -51,7 +58,7 @@ const Food = () => {
     );
   return (
     <div>
-      <SearchContext.Provider value={{searchValue, setSearchValue}}>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <Categories />
         <Sort />
